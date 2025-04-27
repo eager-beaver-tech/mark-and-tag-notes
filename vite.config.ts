57a2925ea5d3e87c088e4578b787a07dc0114f14
BONTERRATECH\c-abey.velangadan <c-abey.velangadan@bonterratech.com>
2025-04-27 10:47:23 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/mark-and-tag-notes/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
